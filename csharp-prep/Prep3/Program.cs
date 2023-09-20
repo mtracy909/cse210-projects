b1f33c5c47386f4e3b2b09ed5adfc27664c3d45e
mtracy909 <mtracy909@gmail.com>
2023-09-19 21:19:32 -0600

    completed prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        string response;
+        int answer = 0;
+        do
+        {
+            Console.Write("What is the magic number? ");
+            response = Console.ReadLine();
+            answer = int.Parse(response);
+            if (answer > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (answer < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.Write("You got it!");
+            }
+        } while (answer != number);
     }
 }
