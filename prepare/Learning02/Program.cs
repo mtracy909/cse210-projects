0595896745935044aeecc2d74b86b85548021a35
mtracy909 <mtracy909@gmail.com>
2023-09-27 23:49:52 -0600

    completed learning activity 02 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Walmart";
+        job1._jobTitle = "Manager";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._company = "Target";
+        job2._jobTitle = "Cashier";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Michael Tracy";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
 }
