45e336f444a860cd8944ed75eaabd165e199238a
mtracy909 <mtracy909@gmail.com>
2023-11-16 14:23:51 -0700

    completed learning activity 03 assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine($"{fraction1.GetTop()}/{fraction1.GetBottom()}");
+        Console.WriteLine($"{fraction2.GetTop()}/{fraction2.GetBottom()}");
+        Console.WriteLine($"{fraction3.GetTop()}/{fraction3.GetBottom()}");
+        fraction3.SetTop(2);
+        fraction3.SetBottom(9);
+        Console.WriteLine($"{fraction3.GetTop()}/{fraction3.GetBottom()}");
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction2.GetDecimalValue());
     }
 }
